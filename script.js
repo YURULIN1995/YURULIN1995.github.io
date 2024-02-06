e43014680e64f5e99c05c46732e9595f36872a6e
YURULIN1995 <xd22kkb22likex@gmail.com>
2024-02-07 01:38:38 +0800

    Update index.html. Delete about.html. Create style.css and script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+function downloadFreeGuide() {
+    // Add logic for free download action
+}
+
+function scheduleFreeConsultation() {
+    // Add logic for scheduling free consultation action
+}
+
+function purchaseCoachingSession() {
+    // Add logic for purchasing coaching session action
+}
