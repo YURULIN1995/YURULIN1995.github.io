b94c718ffabc77629438e5fa7aba870a001b95c4
YURULIN1995 <97010503+YURULIN1995@users.noreply.github.com>
2024-03-23 18:53:12 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+var header = document.querySelector('header');
+var content = document.querySelector('header + .content');
+var stickyOffset = header.offsetTop;
 
+function showText(text) {
+  var elem = document.getElementById('showText');
+  elem.innerText = text;
+}
+
+showText('header offsetTop = ' + stickyOffset + ', pageYOffset = ' + window.pageYOffset);  
+
+
+window.onscroll = function () {
+    showText('header offsetTop = ' + stickyOffset + ', pageYOffset = ' + window.pageYOffset);
+  };
