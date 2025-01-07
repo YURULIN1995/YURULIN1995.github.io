3d81b58f79d707b718a411e0f9cf9453795fce45
YURULIN1995 <97010503+YURULIN1995@users.noreply.github.com>
2025-01-07 17:46:28 +0800

    Update common-header-and-footer.js

--- common-header-and-footer.js
+++ common-header-and-footer.js
@@ -1,11 +1,11 @@
 // JavaScript to include header and footer
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://mind-emulation.uk/header.html")
+  fetch("header.html")
     .then(response => response.text())
     .then(data => {
       document.getElementById("header").innerHTML = data;
     });
-  fetch("https://mind-emulation.uk/footer.html")
+  fetch("footer.html")
     .then(response => response.text())
     .then(data => {
       document.getElementById("footer").innerHTML = data;
