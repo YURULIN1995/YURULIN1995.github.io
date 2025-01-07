b857be1d285b1f0e07e3943bdea1e132062de97e
YURULIN1995 <97010503+YURULIN1995@users.noreply.github.com>
2025-01-07 17:42:37 +0800

    Update main.js

--- main.js
+++ main.js
@@ -11,17 +11,3 @@ showText('header offsetTop = ' + stickyOffset + ', pageYOffset = ' + window.page
 window.onscroll = function () {
     showText('header offsetTop = ' + stickyOffset + ', pageYOffset = ' + window.pageYOffset);
   };
-
-// JavaScript to include header and footer
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("header.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("header").innerHTML = data;
-                });
-  fetch("footer.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("footer").innerHTML = data;
-                });
-        });
